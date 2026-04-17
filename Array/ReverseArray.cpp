582b5e06270e06c3a8325cafebd90d213d1081d3
IshuKarnAru <108353531+IshuKarnAru@users.noreply.github.com>
2026-04-17 12:09:45 +0530

    ok

--- Array/ReverseArray.cpp
+++ Array/ReverseArray.cpp
@@ -1,8 +1,8 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-
-void reverseArray(int arr[], int n){
+void reverseArray(int arr[], int n)
+{
     // for (int i = 0; i <= n / 2 - 1; i++) {
     //     int temp = arr[i];
     //     arr[i] = arr[n - 1 - i];
@@ -12,21 +12,25 @@ void reverseArray(int arr[], int n){
     int start = 0;
     int end = n - 1;
 
-    while (start <= end){
+    while (start <= end)
+    {
         swap(arr[start], arr[end]);
         start++;
         end--;
     }
 }
 
-void printArray(int arr[], int n){
-    for (int i = 0; i <= n - 1; i++) {
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-int main (){
+int main()
+{
     int arr1[6] = {34, 98, 24, 53, 14, 92};
     int arr2[7] = {60, 29, 61, 80, 46, 32, 19};
 
